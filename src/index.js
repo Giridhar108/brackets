0ecffe84bef97c082b0bafbfd537cc4067c2dbc3
German <pentagram777@mail.ru>
2020-10-14 15:03:08 +0500

    'fix: add solutions 3 test'

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-    
-    let brackets = "[]{}()<>"
-    let stack = []
-  
-    for(let bracket of str) {
-      let bracketsIndex = brackets.indexOf(bracket)
-  
-      if (bracketsIndex === -1){
-        continue
-      }
-  
-      if(bracketsIndex % 2 === 0) {
-        stack.push(bracketsIndex + 1)
-      } else {
-        if(stack.pop() !== bracketsIndex) {
-          return false;
+module.exports = function check (str, bracketsConfig) {
+    let openBrackets = [];
+    let closeBrackets = [];
+    bracketsConfig.map (bracketsPair => {
+        openBrackets.push(bracketsPair[0]);
+        closeBrackets.push(bracketsPair[1]);
+    });
+
+    let stack = [];
+    for (let item of str) {
+        if (stack[0] === item) {
+            stack.shift();
+        } else if ((openBrackets.indexOf(item) > -1)) {
+            stack.unshift(closeBrackets[openBrackets.indexOf(item)]);
+        } else {
+            return false;
         }
-      }
     }
-    return stack.length === 0
+    return (stack.length === 0);
 }
